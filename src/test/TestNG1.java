86a844baca43363aa9487e20a42f5083d220ac8d
MaheshKafle <mahesh.kafle@lisec.com>
2024-12-15 23:51:08 +0400

    New TestNG Repo

---
+++ src/test/TestNG1.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestNG1 
+{
+
+//	public static void main(String[] args) 
+//	{
+	
+
+//	}
+	@Test
+	public void Demo()
+	{
+		System.out.println("Hello");
+	}
+
+}
